24a7b714650d3f0a30cbd537efc38f23ddaf26df
Laken Harville <rlakenharville@gmail.com>
2021-09-30 01:16:46 -0500

    Changed to string interpolation.

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -8,16 +8,16 @@ namespace MethodExercise
         {
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
-            Console.WriteLine("Hi, " + name + ". It's nice to meet you."); 
+            Console.WriteLine($"Hi, {name}. It's nice to meet you."); 
             Console.WriteLine("What is your favorite color?");
             string color = Console.ReadLine();
-            Console.WriteLine(color + ". That's my favorite too!");
+            Console.WriteLine($"{color}. That's my favorite too!");
             Console.WriteLine("What's your favorite animal?");
             string animal = Console.ReadLine();
-            Console.WriteLine(animal + "? That's such a strange choice, but alright.");
+            Console.WriteLine($"{animal}? That's such a strange choice, but alright.");
             Console.WriteLine("Alright, now who is your favorite band?");
             string band = Console.ReadLine();
-            Console.WriteLine(band + ". You know, I've never even heard of them.");
+            Console.WriteLine($"{band}. You know, I've never even heard of them.");
             
         }
    
