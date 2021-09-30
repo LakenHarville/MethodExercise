934b433cb1945c5bb381f9ce2fbc54a9e3456631
Laken Harville <rlakenharville@gmail.com>
2021-09-30 00:58:14 -0500

    initial commit

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi, " + name + ". It's nice to meet you."); 
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine(color + ". That's my favorite too!");
+            Console.WriteLine("What's your favorite animal?");
+            string animal = Console.ReadLine();
+            Console.WriteLine(animal + "? That's such a strange choice, but alright.");
+            Console.WriteLine("Alright, now who is your favorite band?");
+            string band = Console.ReadLine();
+            Console.WriteLine(band + ". You know, I've never even heard of them.");
+            
+        }
+   
+    }
+}
